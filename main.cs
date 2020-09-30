767b41f11092f1069a5e2e4feeb8bbafc0c0da2c
snystrom <sophiekarennystrom@gmail.com>
2020-09-30 23:47:42 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,26 @@
+using System;
+
+class MainClass {
+  public static void Main (string[] args) {
+
+  double counter = 1;
+  double total = 0;
+  while (counter <= 7) 
+  {
+  Console.WriteLine ("Enter the distance covered by car #" + counter);
+  double distance = Convert.ToDouble (Console.ReadLine());
+  Console.WriteLine ("Enter the time taken by car #" + counter);
+  double time = Convert.ToDouble (Console.ReadLine());
+  var speed = distance/time;
+  Console.WriteLine("The speed of car #" + counter + " is " + speed + " miles per hour");
+  total = total + speed;
+  counter++;
+  }
+ 
+ var average = total/7;
+ Console.WriteLine ("The average speed of the cars is " + average + " miles per hour");
+  }
+
+  
+  
+}
